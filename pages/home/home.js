f1a95ef63a2191d2c33749ac47594b7f3e3ee6bf
MXHao <1050060266@qq.com>
2020-04-16 16:55:44 +0800

    123

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-list: {}
+    list: {},
+    text1:"",
+    text2:"",
+    result: ""
   },
 
   /**
@@ -13,15 +16,38 @@ list: {}
    */
   onLoad: function (options) {
     var that = this
-wx.request({
-  url: 'https://www.fastmock.site/mock/115d876b2fe8850e5185ea824064d585/Group/GroupList',
-  success(res) {
-    console.log(res.data.GroupList)
-    that.setData({
-list: res.data.GroupList
+    wx.request({
+      url: 'https://www.fastmock.site/mock/115d876b2fe8850e5185ea824064d585/Group/GroupList',
+      success(res) {
+        console.log(res.data.GroupList)
+        that.setData({
+          list: res.data.GroupList
+        })
+      }
     })
-  }
-})
   },
-
+  text1Input(e) {
+    this.setData({
+      text1: e.detail.value
+    })
+  },
+  text2Input(e) {
+    this.setData({
+      text2: e.detail.value
+    })
+      },
+  calc() {
+    var text1 = this.data.text1;
+    var text2 = this.data.text2;
+    if (text2 > text1) {
+      this.setData({
+        result: text2 - text1
+      })
+    }
+    if (text1 >= text2) {
+      this.setData({
+        result: 15
+      })
+    }
+  }
 })
